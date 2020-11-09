9e72969c53312c5b621202b3c639b37154849426
tech-exlorer <smartydheeraj@gmail.com>
2020-11-09 12:59:57 +0530

    inital commit

---
+++ src/test/java/com/spring/boot/git/SpringBootGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
